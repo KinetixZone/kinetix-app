db0dd0913c9e75c937140a57047e6f6648ca12f3
KinetixZone <kinetix.zone@gmail.com>
2026-02-05 13:55:38 -0600

    OmniSync DNA v40.0 [PROD FULL]

--- components/admin/ExerciseLibrary.tsx
+++ components/admin/ExerciseLibrary.tsx
@@ -1,1 +1,1 @@
-import React from 'react'; export const ExerciseLibrary = () => <div className="p-10"><h1>Exercise Lab</h1></div>;
+import React from 'react'; export const ExerciseLibrary = () => null;
