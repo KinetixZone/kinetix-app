60d157d7e9dc25379555bae014b1dd95e237c81b
KinetixZone <kinetix.zone@gmail.com>
2026-02-05 13:13:55 -0600

    OmniSync Mirroring v13.0.0 [TOTAL ADN]

--- components/coach/WorkoutManager.tsx
+++ components/coach/WorkoutManager.tsx
@@ -1,1 +1,1 @@
-import React from 'react'; export const WorkoutManager = () => <div className="p-10">Manager Activo</div>;
+import React from 'react'; export const WorkoutManager = () => <div className="p-10"><h1>Routine Manager</h1></div>;
