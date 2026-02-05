a9979486c87947159ba97f000bf46828792ce8b0
KinetixZone <kinetix.zone@gmail.com>
2026-02-05 14:08:56 -0600

    OmniSync Master DNA v60.0 [TOTAL SYNC]

--- index.tsx
+++ index.tsx
@@ -1,8 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("FATAL: No se encontró el elemento #root");
-const root = ReactDOM.createRoot(rootElement);
-root.render(<React.StrictMode><App /></React.StrictMode>);
+import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App'; const root = ReactDOM.createRoot(document.getElementById('root')!); root.render(<React.StrictMode><App /></React.StrictMode>);
