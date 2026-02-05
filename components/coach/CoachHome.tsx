076228aeded621ea2e6b27532da773cfd93e7f38
KinetixZone <kinetix.zone@gmail.com>
2026-02-05 13:13:53 -0600

    OmniSync Mirroring v13.0.0 [TOTAL ADN]

--- components/coach/CoachHome.tsx
+++ components/coach/CoachHome.tsx
@@ -1,1 +1,1 @@
-import React from 'react'; export const CoachHome = () => <div className="p-10">Coach Terminal</div>;
+import React from 'react'; export const CoachHome = ({ onViewChange }) => <div className="p-10"><h1>Cuartel General</h1><button onClick={() => onViewChange('admin_dashboard')} className="p-4 bg-blue-600 rounded-xl mt-4">Consola Master</button></div>;
