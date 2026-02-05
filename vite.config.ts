cf43368ffd073ade30c997386598a2077d5a4801
KinetixZone <kinetix.zone@gmail.com>
2026-02-05 11:15:37 -0600

    Full System Replicate

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,1 @@
-import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig({ plugins: [react()] });
+import { defineConfig, loadEnv } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig(({ mode }) => { const env = loadEnv(mode, process.cwd(), ''); return { plugins: [react()], define: { 'process.env': JSON.stringify(env) }, build: { outDir: 'dist', sourcemap: false, minify: 'esbuild', target: 'es2020' } }; });
