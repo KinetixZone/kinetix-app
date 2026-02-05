bb60eddf2caaf43a3a7a69ed72b8a39a17beb781
KinetixZone <kinetix.zone@gmail.com>
2026-02-05 11:58:21 -0600

    Full System Sync

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,1 @@
-import { defineConfig, loadEnv } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig(({ mode }) => { const env = loadEnv(mode, process.cwd(), ''); return { plugins: [react()], define: { 'process.env': JSON.stringify(env) }, build: { outDir: 'dist', target: 'es2020' } }; });
+import { defineConfig, loadEnv } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig(({ mode }) => { const env = loadEnv(mode, process.cwd(), ''); return { plugins: [react()], define: { 'process.env': JSON.stringify(env) } }; });
