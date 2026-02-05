97f793b9e421f3368ea71355303dcd058d717ac8
KinetixZone <kinetix.zone@gmail.com>
2026-02-05 13:17:23 -0600

    OmniSync Mirroring v14.0.0 [TOTAL ADN]

--- components/player/AthleteHome.tsx
+++ components/player/AthleteHome.tsx
@@ -1,1 +1,1 @@
-import React from 'react'; export const AthleteHome = () => <div className="p-10"><h1>Home del Atleta</h1></div>;
+import React from 'react'; export const AthleteHome = () => <div className="p-10"><h1>Athlete Terminal Real</h1></div>;
